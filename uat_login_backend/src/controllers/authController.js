280e6b121fc2c634b0be5975cd88c1e6ef95c039
David Castagneto <davidcastagnetoa@gmail.com>
2024-03-25 01:40:57 +0100

    feat(MS Azure Server Authentifation)

--- uat_login_backend/src/controllers/authController.js
+++ uat_login_backend/src/controllers/authController.js
@@ -4,11 +4,14 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-// Esta funcion recibe el usuario y contraseña del la funcion login del cliente, en AuthProvider.js, y si son correctos genera y devuelve un token. Si no son correctos devuelve un 401.
+// Esta funcion recibe el usuario y contraseña del la funcion login del cliente,
+// en AuthProvider.js, y si son correctos genera y devuelve un token.
+// Si no son correctos devuelve un 401.
 export const login = async (req, res) => {
   const { username, password } = req.body;
 
-  // Aquí verificarías contra una base de datos, para este ejemplo compararemos con las variables de entorno
+  // Aquí verificarías contra una base de datos,
+  // para este ejemplo compararemos con las variables de entorno
   if (username === process.env.USER_NAME && password === process.env.USER_PASSWORD) {
     // Generar un token
     const token = jwt.sign({ username }, process.env.JWT_SECRET, { expiresIn: "1h" });
@@ -20,7 +23,10 @@ export const login = async (req, res) => {
   }
 };
 
-//Esta funcion verifica el token recibido por el cliente desde AuthProvider.js en el header de la peticion, y si es valido devuelve el usuario y el token. Si no es valido devuelve un 401.
+// Esta funcion verifica el token recibido por el cliente desde
+// AuthProvider.js en el header de la peticion,
+// y si es valido devuelve el usuario y el token.
+// Si no es valido devuelve un 401.
 export const verifyTokenController = (req, res) => {
   try {
     const authHeader = req.headers.authorization;
--- uat_login_backend/src/routes/authRoutes.js
+++ uat_login_backend/src/routes/authRoutes.js
@@ -1,8 +1,8 @@
 import express from "express";
 import { login, verifyTokenController } from "../controllers/authController.js";
-import passport from "passport";
 import { OAuth2Client } from "google-auth-library";
 import jwt from "jsonwebtoken";
+import { ConfidentialClientApplication } from "@azure/msal-node";
 
 const router = express.Router();
 
@@ -11,19 +11,27 @@ router.post("/verifyToken", verifyTokenController);
 
 const CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 const CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+const MS_CLIENT_ID = process.env.MICROSOFT_APP_CLIENT_ID;
+const MS_CLIENT_SECRET = process.env.MICROSOFT_APP_CLIENT_SECRET;
+const MS_TENANT_ID = process.env.MICROSOFT_APP_TENANT_ID;
 
-// console.debug("CLIENT_ID:", CLIENT_ID);
-// console.debug("CLIENT_SECRET:", CLIENT_SECRET);
+console.debug("CLIENT_ID:", CLIENT_ID);
+console.debug("CLIENT_SECRET:", CLIENT_SECRET);
+console.debug("MS_CLIENT_ID:", MS_CLIENT_ID);
+console.debug("MS_CLIENT_SECRET:", MS_CLIENT_SECRET);
+console.debug("MS_TENANT_ID:", MS_TENANT_ID);
 
 if (!CLIENT_ID) {
   console.error("Google client ID environment variable not set. Please add GOOGLE_CLIENT_ID to .env file.");
   throw new Error("Google client ID environment variable not set");
 }
 
-const oAuth2Client = new OAuth2Client(CLIENT_ID, CLIENT_SECRET, "postmessage");
-// console.debug("Valor del objeto oAuth2Client: " + JSON.stringify(oAuth2Client));
+// ******************************
+// Autenticación OAuth con Google
+// ******************************
 
-// Rutas de autenticación OAuth con Passport
+const oAuth2Client = new OAuth2Client(CLIENT_ID, CLIENT_SECRET, "postmessage"); //Verificar que es "postmessage "
+console.debug("Valor del objeto oAuth2Client: " + JSON.stringify(oAuth2Client));
 router.post("/auth/google", async (req, res) => {
   const { code } = req.body; // Esperamos recibir un 'code', no un 'token'
   console.debug("Contenido de body: " + JSON.stringify(req.body));
@@ -32,6 +40,7 @@ router.post("/auth/google", async (req, res) => {
     const { tokens } = await oAuth2Client.getToken(code);
     console.log("Tokens recibidos: ", tokens);
 
+    //Este await es importante. De lo contrario la funcion getPayload no podra ser llamada
     const ticket = await oAuth2Client.verifyIdToken({
       idToken: tokens.id_token,
       audience: CLIENT_ID,
@@ -58,7 +67,6 @@ router.post("/auth/google", async (req, res) => {
     res.status(401).send("Unauthorized");
   }
 });
-
 router.post("/auth/google/refresh-token", async (req, res) => {
   try {
     const user = new UserRefreshClient(CLIENT_ID, CLIENT_SECRET, req.body.refreshToken);
@@ -69,5 +77,44 @@ router.post("/auth/google/refresh-token", async (req, res) => {
     res.status(401).send("Unauthorized");
   }
 });
+
+// ******************************
+// Autenticación con Microsoft
+// ******************************
+
+const msalConfig = {
+  auth: {
+    clientId: MS_CLIENT_ID, //MICROSOFT_APP_CLIENT_ID
+    clientSecret: MS_CLIENT_SECRET, //MICROSOFT_APP_CLIENT_SECRET
+    authority: `https://login.microsoftonline.com/${MS_TENANT_ID}`,
+  },
+};
+
+// const pca = new ConfidentialClientApplication(msalConfig);
+
+// router.post("/auth/microsoft", async (req, res) => {
+//   const { code } = req.body; // Esperamos recibir un 'code', no un 'token'
+//   // console.debug("Contenido de body: " + JSON.stringify(req.body));
+
+//   try {
+//     const tokenRequest = {
+//       code: code,
+//       scopes: ["User.Read"],
+//       redirectUri: "postmessage",
+//     };
+
+//     const response = await pca.acquireTokenByCode(tokenRequest);
+//     if (response.accessToken) {
+//       console.log("Access token:", response.accessToken);
+//       res.json({ accessToken: response.accessToken });
+//     }
+
+//     // Completa el codigo para generar un token usando el email de Microsft del usuario y su nombre con jwt
+//     // ademas debe responder en formato json con el token generado y la informacion del usuario
+//   } catch (err) {
+//     console.log(err);
+//     res.status(500).send(err);
+//   }
+// });
 
 export default router;
